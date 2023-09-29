87c6ee9bea9a59dbce111b3c4f48bf7f55f687ed
stanomunesh <stanleymunene408@gmail.com>
2023-09-30 01:44:03 +0300

    created a form

---
+++ Php-pdo-database/user-form.php
@@ -0,0 +1,12 @@
+<form action="process.php" method="post">
+  <input type="text" name="name" placeholder="Name">
+  <select name="gender">
+    <option value="male">Male</option>
+    <option value="female">Female</option>
+    <option value="other">Other</option>
+  </select>
+  <input type="date" name="dob" placeholder="Date of birth">
+  <input type="email" name="email" placeholder="Email">
+  <input type="tel" name="phone_number" placeholder="Phone number">
+  <input type="submit" value="Submit">
+</form>
